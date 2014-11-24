6d4025a92ea2497166b0e69ce3297ea1e59f5334
Barna Zoboki <back.barna@gmail.com>
2014-11-24 16:43:59 +0100

    first

---
+++ CCNA_solver/Win32.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace WpfAutomation
+{
+    internal static class Win32
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern short GlobalAddAtom(string lpString);
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern short GlobalDeleteAtom(short nAtom);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam,IntPtr lParam);
+
+
+        public const int MOD_ALT = 1;
+        public const int MOD_CONTROL = 2;
+        public const int MOD_SHIFT = 4;
+        public const int MOD_WIN = 8;
+        public const uint VK_KEY_C = 0x43;
+        public const int WM_HOTKEY = 0x312;
+        public const int VK_CONTROL = 0x11;
+        public const int VK_ALT = 0x12;
+        public const int WM_DRAWCLIPBOARD = 0x308;
+    }
+}
